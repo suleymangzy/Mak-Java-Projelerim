347e3bef367bbc5f5f3caa7ca99a8b7c78de254b
Süleyman Güzey <117369526+suleymangzy@users.noreply.github.com>
2022-11-08 21:25:46 +0300

    Add files via upload

---
+++ DENEME/src/deneme/Deneme.java
@@ -0,0 +1,128 @@
+
+// MANTIKSAL OPERATÖRLER
+// İF ELSE 'NİN KULLANIMI
+// SWİTCH CASE 'NİN KULLANIMI
+
+
+package deneme;
+
+
+import java.util.Scanner;
+
+public class Deneme {
+
+   
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        // MANTIKSAL OPERATÖRLER
+        /*    
+    int a =50;
+    int b =50;
+    int d =10;
+    int c =20;
+         */
+ /*
+        boolean kosul1 = (a>b);
+    boolean kosul2 = (d<c);
+    boolean sonuc = !(kosul1 && kosul2);
+   
+   System.out.println(sonuc);
+     boolean sonuc2 = (a>=b)&& (d<c);
+    System.out.println(sonuc2); 
+         */
+        // İF VE ELSE KULLANIMI  
+      
+        /*
+   if(a==b){
+    System.out.println("a ve b birbirine eşitir");
+    }
+    else {
+    System.out.println("a ve b sayıları eşit değildir");
+    }
+         */
+ /* 
+    if(a<d){
+        System.out.println("d,a'dan büyüktür.");
+    }
+    else if (a<=d){
+        System.out.println("a,d ye eşitir.");
+    }
+    else {
+        System.out.println("a,d'den büyüktür");
+    }
+         */
+       
+
+         // SWİTCH CASE KULLANIMI
+        /*
+   
+   System.out.println("Sayı giriniz:");
+
+   
+   int sayi = scan.nextInt();
+    switch(sayi) {
+       case 1:
+           System.out.println("Sayı 1'e eşittir.");
+      break;
+       case 2:
+           System.out.println("Sayı 2'ye eşitir.");
+      break;
+       case 3:
+           System.out.println("Sayı 3'e eşitir.");
+      break;
+       default: 
+           System.out.println("Geçersiz değer girdiniz.");
+    }
+    
+         */
+        /*    
+        String k_adi, sifre;
+        System.out.println("Kullanıcı adınızı giriniz.");
+        k_adi = scan.nextLine();
+        System.out.println("Şifrenizi giriniz.");
+        sifre = scan.nextLine();
+        if (k_adi.equals("java") && sifre.equals("123")) {
+            System.out.println("Başarılı bir şekilde giriş yaptınız.");
+        } else {
+            System.out.println("Şifreniz veya kullanıcı adınız yanlış"); 
+        }
+         
+    double AVM = Math.pow(2,3);
+        System.out.println(AVM);
+       */
+    
+    }
+}
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
