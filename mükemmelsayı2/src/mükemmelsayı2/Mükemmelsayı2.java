26781e60d121fe0dc9007f6d5ccf395dbaab11a8
Süleyman Güzey <117369526+suleymangzy@users.noreply.github.com>
2022-12-01 19:18:48 +0300

    Add files via upload
    klavyeden girilen pozitif tamsayı üst sınır  için MS=2^n((2^n+1)-1) çarpımıyla elde edilen sayıların ((2^n+1)-1)kısmı asal sayı ise ,çarpım sonucu "mükemmel sayı" dır. 
    yukarıdaki açıklamaya göre mükemmel sayıların listesini veren program

---
+++ mükemmelsayı2/src/mükemmelsayı2/Mükemmelsayı2.java
@@ -0,0 +1,16 @@
+
+package mükemmelsayı2;
+
+import java.util.Scanner;
+public class Mükemmelsayı2 {
+
+   
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+        System.out.println("üst sınırı giriniz.");
+        int n = input.nextInt();
+      
+    
+    }
+    
+}
