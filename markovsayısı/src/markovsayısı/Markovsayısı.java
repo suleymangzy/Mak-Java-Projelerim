eb736a0d374bd5eeb0eb800d476a5d1a7bc2b329
Süleyman Güzey <117369526+suleymangzy@users.noreply.github.com>
2022-12-02 14:51:15 +0300

    Add files via upload
    x,y,z elemandır [1,50] için "markov sayıları" nı listeleyen program

---
+++ markovsayısı/src/markovsayısı/Markovsayısı.java
@@ -0,0 +1,23 @@
+
+package markovsayısı;
+
+
+public class Markovsayısı {
+
+  
+    public static void main(String[] args) {
+        for (int i = 1; i <=50; i++) {
+            for (int j =i; j <=50; j++) {
+                for (int k = j; k <= 50; k++) {
+                    if ((i*i+j*j+k*k)==(3*i*j*k)) {
+                        System.out.println(i+","+j+","+k);
+                    }
+                }
+                
+            }
+            
+        }
+       
+    }
+    
+}
