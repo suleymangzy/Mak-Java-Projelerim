db57e74703045fb411efb20da7cf29824517455a
Süleyman Güzey <117369526+suleymangzy@users.noreply.github.com>
2023-04-01 14:40:10 +0300

    Update Mükemmelsayı2.java
    "Mükemmel Sayılar" formülü MS:2^(x-1)*((2^x)-1)) `dir. Bu formüle göre ((2^x)-1)) kısmı eğer asal bir sayı ise formülün tamamının sonucu "Mükemmel sayı" `eşit olacaktır. Buna göre kullanıcı tarafından girilen "x" değerine kadar bulunan "Mükemmel Sayılar" `ı sıralayan programın java kodu.

--- mükemmelsayı2/src/mükemmelsayı2/Mükemmelsayı2.java
+++ mükemmelsayı2/src/mükemmelsayı2/Mükemmelsayı2.java
@@ -9,7 +9,20 @@ public class Mükemmelsayı2 {
        Scanner input = new Scanner(System.in);
         System.out.println("üst sınırı giriniz.");
         int n = input.nextInt();
-      
+        System.out.print("Mükemmel sayılar: ");
+        int sayac;
+        for (int i = 2; i <= num; i++) {
+            int islem = (int) (Math.pow(2, i) - 1);
+            sayac = 0;
+            for (int j = 1; j <= islem; j++) {
+                if (islem % j == 0) {
+                    sayac++;
+                }
+            }
+            if (sayac == 2) {
+                System.out.print((int)(Math.pow(2, i - 1) * (Math.pow(2, i) - 1))+",");
+            }
+        }
     
     }
     
